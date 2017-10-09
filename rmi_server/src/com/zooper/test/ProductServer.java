40bcd27f8a6536d61f0cc3cf6b2f9ec2c3d533af
zZooper <1004825259@qq.com>
2017-10-09 22:39:09 +0800

    rmi服务端

--- rmi_server/src/com/zooper/test/ProductServer.java
+++ rmi_server/src/com/zooper/test/ProductServer.java
@@ -12,7 +12,13 @@ public class ProductServer {
 		ProductImpl p1 = new ProductImpl("blackwell toaster");
 		ProductImpl p2 = new ProductImpl("zapXpress Microware Oven");
 		
+		/*
+		 * 本地主机上的远程对象注册表Registry的实例，并指定端口为8888，
+		 * 这一步必不可少（Java默认端口是1099），必不可缺的一步，
+		 * 缺少注册表创建，则无法绑定对象到远程注册表上 
+		 */
 		LocateRegistry.createRegistry(8888);
+		
 		//发布服务
 		Naming.bind("rmi://localhost:8888/toaster",p1); 
 		Naming.bind("rmi://localhost:8888/Microware",p2); 
